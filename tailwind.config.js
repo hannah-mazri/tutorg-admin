b012c65fa32bc75370da93589daa78a4995983e6
hannah <hannahnajwa@gmail.com>
2021-08-23 21:20:53 +0800

    enable jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const { screens, spacing, fontFamily } = require('tailwindcss/defaultTheme');
 module.exports = {
+  mode: 'jit',
   purge: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
